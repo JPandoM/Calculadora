801685e6f24a534c6ab3736edce4a1e36003afe3
Jaicle Pando Moriyón <jpandomoriyon@outlook.es>
2025-10-29 15:24:15 +0100

    Basic tracer as an internal lib

--- src/main/java/com/example/calculadora/service/CalculadoraService.java
+++ src/main/java/com/example/calculadora/service/CalculadoraService.java
@@ -1,15 +1,22 @@
 package com.example.calculadora.service;
 
+import io.corp.calculator.TracerImpl;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CalculadoraService {
 
+    private final TracerImpl tracer = new TracerImpl();
+
     public int suma(int a, int b) {
-        return a + b;
+        int resultado = a + b;
+        tracer.trace(resultado);
+        return resultado;
     }
 
     public int resta(int a, int b) {
-        return a - b;
+        int resultado = a - b;
+        tracer.trace(resultado);
+        return resultado;
     }
 }
